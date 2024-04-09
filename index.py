733610f312b10e091f8f9858edd5921a1fbb843e
MasterofElectronic <ing.jamg23@gmail.com>
2024-04-08 22:44:09 -0500

    Modificacion Index

--- index.py
+++ index.py
@@ -1,60 +1,77 @@
 import base64
 import io
-import torch 
-import numpy as np
-from ultralytics import YOLO
-from flask import Flask, jsonify
+import subprocess
+import time
+from flask import Flask, jsonify, request
 from PIL import Image
+from ultralytics import YOLO
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Ruta de la imagen local
-    image_path = 'C:/Users/jhona/Documents/uis/cowcounting/imagenes/03_24_2024_07_39_16_p.m._Img.jpg'
+
+
+    if request.method == 'GET':
+        # Ruta de la imagen local
+        image_path = 'C:/Users/jhona/Documents/uis/cowcounting/imagenes/vacamodelo.jpg'
 
     # Cargar el modelo con los mejores pesos obtenidos y se define como model para usarlo posteriormente
-    model = YOLO('C:/Users/jhona/Documents/uis/cowcounting/model_cow.pt')
+        model = YOLO('C:/Users/jhona/Documents/uis/cowcounting/model_cow.pt')
 
     # Cargar la imagen localmente
-    image = Image.open(image_path)
+        image = Image.open(image_path)
 
     # Realiza inferencia con el modelo YOLOv8n en la imagen
-    result = model(image)
+        result = model(image)
 
-    num_b = 0  # Variable para número de cajas lo cual representará el número de vacas
-    for r in result:
-        num_b += len(r)
+        num_b = 0  # Variable para número de cajas lo cual representará el número de vacas
+        for r in result:
+            num_b += len(r)
 
     # Generar imagen con las detecciones
-    annotated_img = result[-1].plot()
+        annotated_img = result[-1].plot()
     
     # Convertir la imagen a un objeto PIL.Image
-    annotated_img_pil = Image.fromarray(annotated_img)
+        annotated_img_pil = Image.fromarray(annotated_img)
 
     # Convertir imagen resultante a base64
-    img_buffer = io.BytesIO()
-    annotated_img_pil.save(img_buffer, format="JPEG")
+        img_buffer = io.BytesIO()
+        annotated_img_pil.save(img_buffer, format="JPEG")
 
     # Convertir la imagen resultante a base64
-    img_str = base64.b64encode(img_buffer.getvalue()).decode('utf-8')
+        img_str = base64.b64encode(img_buffer.getvalue()).decode('utf-8')
 
     # Crear el JSON de respuesta
-    response_json = {
-        'image_base64': img_str,
-        'num_vacas': num_b
-    }
+        response_json = {
+            'image_base64': img_str,
+            'num_vacas': num_b
+        }
+        return jsonify(response_json)
+    
+    
+    elif request.method == 'POST':
+        base64_img = request.form['imagen']
+        # Decodificar la imagen Base64
+        img_data = base64.b64decode(base64_img)
 
-    return jsonify(response_json)
+        # Convertir la imagen decodificada a formato de imagen
+        img = Image.open(io.BytesIO(img_data))
+
+        # Guardar la imagen decodificada en una dirección específica
+        img.save('C:/Users/jhona/Documents/uis/cowcounting/imagenes/vacamodelo.jpg')
+        time.sleep(0.1)
+        return 'Respuesta para el método POST'
+
+
+    
+
+
+    
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
 
 
